ef11ea392c40c70561c97fc01822e1c5ee27b255
ziagham <amin.ziagham@outlook.com>
2025-04-12 10:21:46 +0200

    Initializing project #1

---
+++ src/Services/IAmazonS3Connection.cs
@@ -0,0 +1,9 @@
+﻿using Amazon.S3;
+using FlowSynx.Plugins.Amazon.S3.Models;
+
+namespace FlowSynx.Plugins.Amazon.S3.Services;
+
+public interface IAmazonS3Connection
+{
+    AmazonS3Client Connect(AmazonS3Specifications specifications);
+}
