57b81a4fab8beff87a4ddb8a233ec9d1b40b2180
ziagham <amin.ziagham@outlook.com>
2025-06-23 22:48:18 +0200

    Update FlowSynx.PluginCore to 1.3.0 and adapt new changes for that #10

--- src/AmazonS3Plugin.cs
+++ src/AmazonS3Plugin.cs
@@ -21,8 +21,8 @@ public class AmazonS3Plugin : IPlugin
                 Name = "Amazon.S3",
                 CompanyName = "FlowSynx",
                 Description = Resources.PluginDescription,
-                Version = new PluginVersion(1, 1, 0),
-                Namespace = PluginNamespace.Connectors,
+                Version = new PluginVersion(1, 0, 0),
+                Category = PluginCategory.Cloud,
                 Authors = new List<string> { "FlowSynx" },
                 Copyright = "© FlowSynx. All rights reserved.",
                 Icon = "flowsynx.png",
@@ -30,7 +30,6 @@ public class AmazonS3Plugin : IPlugin
                 RepositoryUrl = "https://github.com/flowsynx/plugin-amazon-s3",
                 ProjectUrl = "https://flowsynx.io",
                 Tags = new List<string>() { "FlowSynx", "Amazon", "S3", "Cloud" },
-                Category = PluginCategories.CloudPlatform
             };
         }
     }
